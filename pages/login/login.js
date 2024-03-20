a25e5ef123e8b167bd8f41890095aa5cfa6348b6
Neuxbane <neuxbane@gmail.com>
2024-03-20 21:26:44 +0000

    bypass

--- pages/login/login.js
+++ pages/login/login.js
@@ -9,9 +9,13 @@ onload = () => {
 async function checkForm() {
 	const email = document.getElementById("email").value;
 	const kataSandi = document.getElementById("kataSandi").value;
+	toast('success', data.message);
+	navigate("home");
+
+	// API HAS AN SSL CERTIFICATE ERROR
 	const data = await API.login(email, kataSandi);
 	if (data.success) {
-		toast('success', data.message);
+	toast('success', data.message);
 		storage.set('profile', data.data);
 		return navigate("home");
 	} else {
