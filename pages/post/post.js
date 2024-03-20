6c8f50424a46d136fe7e513edc762f19e636272a
Neuxbane <neuxbane@gmail.com>
2024-03-20 07:43:16 +0700

    update few things

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,4 +1,8 @@
 const upload = ()=>{
+	if (!document.getElementById('titlepost').value || !document.getElementById('imagepost').value || !document.getElementById('captionpost').value) {
+		toast('warning','Failed to upload post\nPlease fill in all required input');
+		return;
+	}
 	navigate('home');
 }
 
